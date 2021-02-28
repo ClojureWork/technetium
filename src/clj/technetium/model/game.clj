937c7d57d421a242a4bef854f35a8993c3559914
Todd Crone <twcrone@gmail.com>
2021-02-28 10:29:41 -0500

    Initialize the asteroid field

--- src/clj/technetium/model/game.clj
+++ src/clj/technetium/model/game.clj
@@ -1,6 +1,6 @@
 (ns technetium.model.game)
 
-(def field-size 3)
+(def field-size 4)
 
 (def field-data {:size   field-size
                  :radioactive [{:x 1 :y 1 :z 1} {:x 0 :y 0 :z 0}]})
@@ -18,12 +18,27 @@
 (defn radiation-from [x y z]
   (if (.contains (field-data :radioactive) {:x x :y y :z z}) 1 0))
 
+(defn adjacent [x y z]
+  (remove #(= {:x x :y y :z z} %)
+          (for [a (range (- x 1) (+ x 2))
+                b (range (- y 1) (+ y 2))
+                c (range (- z 1) (+ z 2))]
+            {:x a :y b :z c})))
+
+(defn radiation-from-location [location]
+  (radiation-from (:x location) (:y location) (:z location)))
+
+(defn residual-radiation-at [x y z]
+  (reduce + (map radiation-from-location (adjacent x y z))))
+
 (defn new-asteroid-field [data]
   (let [size (:size data)]
     (for [x (range 0 size)
           y (range 0 size)
           z (range 0 size)]
-      (asteroid x y z nil))))
+      (asteroid x y z (residual-radiation-at x y z)))))
+
+
 
 (def game (atom []))
 
@@ -39,9 +54,6 @@
         z (range 0 size)]
     (asteroid x y z nil)))
 
-(defn radiation-from-location [location]
-  (radiation-from (:x location) (:y location) (:z location)))
-
 (defn reset []
   (reset! game (new-game field-size)) @game)
 
@@ -49,16 +61,6 @@
   (if
     (.contains (field-data :radioactive) {:x x :y y :z z})
     (reset) (new-game 1)))
-
-(defn adjacent [x y z]
-  (remove #(= {:x x :y y :z z} %)
-          (for [a (range (- x 1) (+ x 2))
-                b (range (- y 1) (+ y 2))
-                c (range (- z 1) (+ z 2))]
-            {:x a :y b :z c})))
-
-(defn residual-radiation-at [x y z]
-  (reduce + (map radiation-from-location (adjacent x y z))))
 
 (defn get-game [] @game)
 
