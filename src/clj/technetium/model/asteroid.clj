30a1048603287da404b4a34b1f53f078b229c7c2
Todd Crone <twcrone@gmail.com>
2021-03-08 19:01:53 -0500

    Refactor to rely less of static data

--- src/clj/technetium/model/asteroid.clj
+++ src/clj/technetium/model/asteroid.clj
@@ -4,13 +4,42 @@
 
 (def field-size 4)
 
-(def ^:private field-data {:size   field-size
+(def ^:private field-data {:size        field-size
                            :radioactive [{:x 1 :y 1 :z 1} {:x 0 :y 0 :z 0}]})
 
 (def ^:private field (atom []))
 
-(defn radioactive? [x y z]
-  (some #(= % {:x x :y y :z z}) (:radioactive field-data)))
+(defn radioactive?
+  ([data x y z] (some #(= % {:x x :y y :z z}) (:radioactive data)))
+  ([x y z] (radioactive? field-data x y z)))
+
+(defn adjacent [x y z]
+  (remove #(= {:x x :y y :z z} %)
+          (for [a (range (- x 1) (+ x 2))
+                b (range (- y 1) (+ y 2))
+                c (range (- z 1) (+ z 2))]
+            {:x a :y b :z c})))
+
+(defn count-radioactive-neighbors [data x y z]
+  (count (filter #(radioactive? data (:x %) (:y %) (:z %)) (adjacent x y z))))
+
+(defn radiation-at [data x y z]
+  (if (radioactive? data x y z) RADIOACTIVE (count-radioactive-neighbors data x y z)))
+
+(defn asteroid [data x y z]
+  {:x         x
+   :y         y
+   :z         z
+   :radiation (radiation-at data x y z)})
+
+(defn new-asteroid-field
+  ([data]
+   (let [size (:size data)]
+     (for [x (range 0 size)
+           y (range 0 size)
+           z (range 0 size)]
+       (asteroid data x y z))))
+  ([] (new-asteroid-field field-data)))
 
 (defn new-asteroid
   ([x y z] (new-asteroid x y z nil))
@@ -20,27 +49,6 @@
     :z         z
     :radiation radiation
     :flagged   false}))
-
-(defn adjacent [x y z]
-  (remove #(= {:x x :y y :z z} %)
-          (for [a (range (- x 1) (+ x 2))
-                b (range (- y 1) (+ y 2))
-                c (range (- z 1) (+ z 2))]
-            {:x a :y b :z c})))
-
-(defn radiation-at [x y z]
-  (if (radioactive? x y z)
-    RADIOACTIVE
-    (count (filter #(radioactive? (:x %) (:y %) (:z %)) (adjacent x y z)))))
-
-(defn new-asteroid-field
-  ([] (new-asteroid-field field-data))
-  ([data]
-   (let [size (:size data)]
-     (for [x (range 0 size)
-           y (range 0 size)
-           z (range 0 size)]
-       (new-asteroid x y z (radiation-at x y z))))))
 
 (defn has-location [asteroid x y z]
   (and (= (asteroid :x) x) (= (asteroid :y) y) (= (asteroid :z) z)))
@@ -52,7 +60,7 @@
   ([x y z field] (first (filter #(has-location % x y z) field))))
 
 (defn reset []
-  (reset! field (new-asteroid-field)) @field)
+  (reset! field (new-asteroid-field field-data)) @field)
 
 (defn radioactive-count [field]
   (count (filter #(= RADIOACTIVE (:radiation %)) field)))
